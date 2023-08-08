3f4e5667b47e5acd591878a89ac540202484e9a5
yeaktonghan <yeaktonghan.github@gmail.com>
2023-08-08 11:59:40 +0700

    first commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            Hi Everyone, I am <span className="purple">Han Yeaktong </span>
+            from <span className="purple"> Phnom Penh, Cambodia.</span>
+            <br /> I get this project from GitHub
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            It's NOT mine.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+         
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "YOLO"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">me</footer>
         </blockquote>
       </Card.Body>
     </Card>
